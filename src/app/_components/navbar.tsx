8d781fae52d36896410d5397f315dfecd57f09ac
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2024-01-30 15:49:25 +0700

    feat: navbar + responsive

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -6,9 +6,9 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
-export default function SideBar() {
+export function NavBar() {
   return (
-    <nav className="fixed left-0 top-0 z-50 flex h-screen w-[100px] flex-col gap-[10px] bg-white px-6 py-9 shadow-sm">
+    <nav className="sticky left-0 top-0 z-50 hidden h-screen w-[100px] flex-col gap-[10px] bg-white px-6 py-9 shadow-sm md:flex">
       <div className="flex h-[762px] flex-col gap-9">
         <Image src="/svgs/logo-default.svg" width={62} height={46} alt="logo" />
         <div className="group relative flex h-8 w-8 flex-row items-center self-center hover:cursor-pointer">
@@ -26,6 +26,36 @@ export default function SideBar() {
           <FontAwesomeIcon icon={faComment} className="h-8 w-8 text-medium" />
           <div className="absolute left-[62px] hidden h-10 w-1 rounded-sm bg-primary-500 group-hover:block"></div>
         </div>
+      </div>
+      <div className="items-center justify-center self-center">
+        <div className="relative flex h-10 w-10">
+          <Image
+            src="/images/feed/mock-profile/mock-1.jpg"
+            fill
+            alt="logo"
+            className="self-center rounded-full"
+            objectFit="cover"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export function NavBarMobile() {
+  return (
+    <nav className=" z-50 mx-auto flex h-14 w-[80%] justify-between rounded-3xl bg-white px-9 md:hidden">
+      <div className="h-8 w-8 flex-row items-center self-center hover:cursor-pointer">
+        <FontAwesomeIcon icon={faCompass} className="h-8 w-8 text-medium" />
+      </div>
+      <div className="h-8 w-8 flex-row items-center self-center hover:cursor-pointer">
+        <FontAwesomeIcon
+          icon={faCalendarCheck}
+          className="h-8 w-8 text-medium"
+        />
+      </div>
+      <div className="h-8 w-8 flex-row items-center self-center hover:cursor-pointer">
+        <FontAwesomeIcon icon={faComment} className="h-8 w-8 text-medium" />
       </div>
       <div className="items-center justify-center self-center">
         <div className="relative flex h-10 w-10">
--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,20 +1,23 @@
 import { Input } from "@/components/ui/input";
 import { Typography } from "@/components/ui/typography";
 import Card from "@/app/_components/feed/card";
-import SideBar from "../_components/sidebar";
+import { NavBar, NavBarMobile } from "../_components/navbar";
 import { faCompass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function FeedPage() {
   return (
-    <div className="flex h-auto w-screen flex-row bg-bgColor">
-      <SideBar />
-      <div className="flex w-full flex-col gap-4 p-9 sm:ml-[100px]">
+    <div className="flex h-auto flex-row bg-bgColor">
+      <NavBar />
+      <div className="fixed bottom-4 z-50 w-full justify-center ">
+        <NavBarMobile />
+      </div>
+      <div className="flex w-full flex-col gap-4 p-6 lg:p-9">
         <div className="flex flex-col gap-4 self-stretch">
           <Header />
           <SearchBar />
         </div>
-        <div className="flex justify-center rounded-xl border border-solid border-primary-300 bg-white p-9 ">
+        <div className="flex justify-center rounded-xl border border-solid border-primary-300 bg-white p-6 lg:p-9 ">
           <CardContainer />
         </div>
       </div>
@@ -25,19 +28,20 @@ export default function FeedPage() {
 function Header() {
   return (
     <div className="flex flex-col justify-center gap-2">
-      <div className="flex items-center gap-[10px]">
+      <div className="relative flex items-center gap-[10px]">
         <FontAwesomeIcon
           icon={faCompass}
-          className="h-10 w-10 text-primary-500"
+          className="h-10 w-10 text-secondary-400"
         />
-        <Typography
-          variant="h2"
-          className="text-4xl leading-9 text-primary-900"
-        >
+        <Typography variant="h2" className="text-primary-900">
           Discover
         </Typography>
+        <div className="absolute right-0 h-12 w-14 items-center rounded-lg bg-primary-500 p-2 md:hidden"></div>
       </div>
-      <Typography variant="h4" className="text-base leading-6 text-primary-700">
+      <Typography
+        variant="h4"
+        className="text-base text-primary-700 lg:text-xl"
+      >
         Unlock a World of Possibilities: Find Friends for Every Adventure on
         HeatHub!
       </Typography>
@@ -47,7 +51,7 @@ function Header() {
 
 function SearchBar() {
   return (
-    <div className="flex flex-row items-center gap-4 self-stretch">
+    <div className="hidden flex-row items-center gap-4 self-stretch md:flex">
       <Input className="h-full p-3" placeholder="Search for friends" />
       <div className="h-12 w-14 rounded-lg bg-primary-500 p-2"></div>
     </div>
@@ -56,7 +60,7 @@ function SearchBar() {
 
 function CardContainer() {
   return (
-    <div className="flex flex-row flex-wrap gap-9">
+    <div className="grid w-full min-w-[280px] grid-cols-1 justify-center gap-6 md:grid-cols-2 lg:grid-cols-3 lg:gap-9">
       <Card />
       <Card />
       <Card />
@@ -64,3 +68,7 @@ function CardContainer() {
     </div>
   );
 }
+
+// <div className="fixed bottom-4 z-50 w-full justify-center ">
+//         <NavBarMobile />
+//       </div>
