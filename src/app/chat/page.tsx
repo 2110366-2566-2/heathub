58d355d56be3ef7addba830644b58f38fa237327
bruce <bruceoio@hotmail.com>
2024-02-03 16:11:12 +0700

    fix:lint

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,23 +1,3 @@
-import { MessageCard } from "./components/MessageCard";
-import { ChatMessage } from "./components/ChatMessage";
-import { MessageShownType } from "./components/type";
 export default async function Page() {
-  return (
-    <div>
-      {/* <MessageCard */}
-      {/*   anotherUserID={"id:1234"} */}
-      {/*   senderName={"Emelie"} */}
-      {/*   messageCount={2} */}
-      {/*   messageTimeStamp={1706876377} */}
-      {/*   lastestMessage="Hello Sawadekub Hello Sawadekub Hello Sawadekub Hello Sawadekub Hello Sawadekub Hello Sawadekub" */}
-      {/* /> */}
-      {/* <ChatMessage */}
-      {/*   senderName="Meen Sankaew" */}
-      {/*   messageShownType={MessageShownType.ALL} */}
-      {/*   message="hellllo my name is boo na youu" */}
-      {/*   createdAt="" */}
-      {/*   imageUrl="https://github.com/shadcn.png" */}
-      {/* ></ChatMessage> */}
-    </div>
-  );
+  return <div></div>;
 }
