7d1aaece9fec068aaba3f00a513551a86d63c8f9
Supawit Saeliew <63398464+JayJacka@users.noreply.github.com>
2024-02-08 10:07:29 +0700

    feat(web): date picker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ export default async function Home() {
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Turtoise <span className="text-[hsl(280,100%,70%)]">not</span> lonely!
         </h1>
+        <DatePicker />
         <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
             {user ? user.firstName : "Loading tRPC query..."}
