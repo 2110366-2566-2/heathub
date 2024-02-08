8f2e5ef938759c0f8741e141844becc4a44fc8ef
Supawit Saeliew <63398464+JayJacka@users.noreply.github.com>
2024-02-08 10:23:36 +0700

    fix: build failed

--- src/app/_components/DatePicker.tsx
+++ src/app/_components/DatePicker.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/utils/tailwind-merge"
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
 import {
@@ -14,7 +14,7 @@ import {
 } from "@/components/ui/popover"
 
 export function DatePicker() {
-  const [date, setDate] = React.useState<Date>(new Date())
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
 
   return (
     <Popover>
