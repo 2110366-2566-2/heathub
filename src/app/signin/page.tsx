d061b5f6ba76c8d3cf589a2c199beb75b9a66b42
Warun03547 <warun212545@gmail.com>
2024-02-09 22:06:57 +0700

    finish components

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,6 +3,10 @@ import { api } from "@/trpc/react";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 import { signIn } from "../../action/auth";
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
+import { Toggle } from "@/components/ui/toggle"
 
 export default function SignIn() {
   const { data: user } = api.auth.me.useQuery();
@@ -26,32 +30,18 @@ export default function SignIn() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-primary-300 to-secondary-200 text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="font-bold">Sign in</h1>
+        <div className="h1-bold">Sign in</div>
 
         <form action={signInHandler} className="flex flex-col gap-4 text-black">
-          <input
-            className="rounded-md p-2"
-            type="text"
-            name="email"
-            placeholder="email"
-          />
-          <input
-            className="rounded-md p-2"
-            type="password"
-            name="password"
-            placeholder="password"
-          />
-
+          <Input type="email" placeholder="Email" />
+          <Input type="password" placeholder="Password" />
           {error && <p className="text-red-500">{error}</p>}
-          <button
-            type="submit"
-            className="rounded-md bg-violet-500 p-4 text-white"
-            disabled={loading}
-          >
+          <Button variant="link">Forgot your password?</Button>
+          <Button type="submit" disabled={loading}>
             Sign In
-          </button>
+          </Button>
         </form>
       </div>
     </main>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,24 +1,36 @@
-import * as React from "react";
+import * as React from "react"
+import { cn } from "@/lib/utils"
 
-import { cn } from "@/utils/tailwind-merge";
+export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  size?: "sm" | "md" | "lg";
+}
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, size = "md", ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className,
+          "flex h-10 w-full rounded-xl border border-primary-300 bg-background px-3 py-2 text-body5 ring-offset-background file:mr-4 file:rounded-xl file:border-0 file:bg-primary-500 file:text-invert file:text-sm file:font-medium hover:file:bg-primary-600 placeholder:text-placeholder focus-visible:outline-none focus-visible:ring focus-visible:ring-primary-100 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:bg-disable disabled:opacity-90 invalid:border-red-500 invalid:bg-red-100 invalid:text-red-500",
+          {
+            'py-3': size === 'lg',
+            'py-1': size === 'sm',
+            'h-12': size === 'lg',
+            'h-8': size === 'sm',
+            'text-body6': size === 'sm',
+          },
+
+          className
         )}
         ref={ref}
         {...props}
       />
-    );
-  },
-);
-Input.displayName = "Input";
+    )
+  }
+)
+Input.displayName = "Input"
 
-export { Input };
+export { Input }
+
