23a2794c1c57690d81bec626b3e203cdb5401869
Punnawat Lohanuit <punnawat58000@gmail.com>
2024-02-09 15:58:07 +0700

    feat:custom gender

--- src/app/register/tellUsAboutYourself/host/_component/ComponentGround.tsx
+++ src/app/register/tellUsAboutYourself/host/_component/ComponentGround.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
@@ -6,17 +6,25 @@ import { useRouter } from "next/navigation";
 import RegisterFormBox from "./RegisterFormBox";
 
 export default function ComponentsGround() {
+  const handleButtonClick = () => {
+    return;
+  };
 
-    const handleButtonClick = () => {
-        return;
-    }
-    
-
-    return (
-        <div className="flex flex-col items-center gap-y-[24px]">
-            <div className="text-primary-900 md:h1-bold h2-bold">Host tell us about yourself</div>
-            <RegisterFormBox/>
-            <Button className="bg-primary-500 text-white" variant="outline" onClick={()=>{handleButtonClick();}}>Next</Button>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center gap-y-[24px]">
+      <div className="md:h1-bold h2-bold text-primary-900">
+        Host tell us about yourself
+      </div>
+      <RegisterFormBox />
+      <Button
+        className="bg-primary-500 text-white"
+        variant="outline"
+        onClick={() => {
+          handleButtonClick();
+        }}
+      >
+        Next
+      </Button>
+    </div>
+  );
 }
