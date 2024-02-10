bd48f5f9c83c7225db42bc6a354aad7aefa55863
bruce <bruceoio@hotmail.com>
2024-02-11 05:04:59 +0700

    feat: add Loading

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { MainMessageComponent } from "./components/MainMessageComponent";
+import { MessageList} from "./components/MessageList";
 export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const isChatMainPage =
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="min-w-screen flex min-h-screen flex-row overflow-x-hidden">
       <nav className="sticky left-0 top-0 z-50 hidden h-screen w-[100px] flex-col justify-between gap-3 bg-blue-500 px-6 py-9 shadow-sm lg:flex"></nav>
-      <MainMessageComponent className={isChatMainPage} />
+      <MessageList className={isChatMainPage} />
       {children}
     </main>
   );
