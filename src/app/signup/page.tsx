f640d40378e348c4ed9277125376efa2598f45cd
Brucennnnn <30439608+Brucennnnn@users.noreply.github.com>
2024-02-12 19:30:25 +0700

    feat(api): change schema back (#38)
    * feat: change schema back
    
    * feat: format

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-
 import { api } from "@/trpc/react";
 import { redirect } from "next/navigation";
 import { useEffect, useRef } from "react";
 
-export default function SignUp() { // Participant
+export default function SignUp() {
+  // Participant
   const signUpUser = api.auth.signupPaticipate.useMutation();
   const { data, isSuccess } = api.auth.getAllUsers.useQuery();
 
@@ -122,29 +122,28 @@ export default function SignUp() { // Participant
   );
 }
 
-function checkDetail(formData:FormData){
-  try{
+function checkDetail(formData: FormData) {
+  try {
     const aka = formData.get("username") as string | null;
     const firstName = formData.get("firstName") as string | null;
     const lastName = formData.get("lastName") as string | null;
     const dateOfBirth = formData.get("dateOfBirth") as string | null;
     const gender = formData.get("gender") as string | null;
-    
-    if(!aka){
-      throw new Error("Missing AKA")
+
+    if (!aka) {
+      throw new Error("Missing AKA");
     }
-    if(!firstName || !lastName){
-      throw new Error("Missing firstName or lastName")
+    if (!firstName || !lastName) {
+      throw new Error("Missing firstName or lastName");
     }
-    if(!gender){
-      throw new Error("Missing gender")
+    if (!gender) {
+      throw new Error("Missing gender");
     }
-    if(!dateOfBirth){
-      throw new Error("MissingBirthDate")
+    if (!dateOfBirth) {
+      throw new Error("MissingBirthDate");
     }
-    return null
-  }
-  catch (error){
-    return (error as Error).message
+    return null;
+  } catch (error) {
+    return (error as Error).message;
   }
 }
