fb37f4546dbebabbc3b6dec302b10cac62762684
bruce <bruceoio@hotmail.com>
2024-02-14 15:29:29 +0700

    fix:lint

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,13 +1,10 @@
-import noChat from "@/../public/svgs/no-chat.svg";
 import Image from "next/image";
 export default async function Page() {
   return (
     <div className="hidden grow flex-col items-center justify-center gap-2 lg:flex xl:flex">
       <div className="h-fit w-fit">
-        <Image priority src={noChat} alt="No chat" />
-        <div className="h2-bold flex  items-center justify-center text-medium">
-          No Message
-        </div>
+        <Image width={150} height={150} src="/svgs/no-chat.svg" alt="No chat" />
+        <div className="h2-bold flex  items-center justify-center text-medium">No Message</div>
       </div>
     </div>
   );
