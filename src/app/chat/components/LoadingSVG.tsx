e4b0a38d1fde2256c11ba832fb187773f9382e25
Supawit Saeliew <63398464+JayJacka@users.noreply.github.com>
2024-02-14 09:55:40 +0700

    fix: change loading svg size

--- src/app/chat/components/LoadingSVG.tsx
+++ src/app/chat/components/LoadingSVG.tsx
@@ -4,8 +4,8 @@ export default function LoadingSVG() {
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       style={{ margin: "auto", background: "none", display: "block" }}
-      width="50px"
-      height="50px"
+      width="40px"
+      height="40px"
       viewBox="0 0 100 100"
       preserveAspectRatio="xMidYMid"
     >
