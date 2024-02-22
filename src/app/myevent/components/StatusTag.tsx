ea350f194ddf273b7e31b933d43e3273407810ee
bpremika <premika.yu@gmail.com>
2024-02-22 16:16:09 +0700

    fix: format prettier

--- src/app/myevent/components/StatusTag.tsx
+++ src/app/myevent/components/StatusTag.tsx
@@ -2,37 +2,37 @@ import { Tag } from "@/app/_components/tag";
 import { EventStatus } from "./Card";
 
 interface StatusProps {
-    size: "sm" | "md" | "lg";
-    status?: EventStatus;
+  size: "sm" | "md" | "lg";
+  status?: EventStatus;
 }
 
-export function StatusTag(props: StatusProps){
-    switch (props.status) {
-      case EventStatus.WAITINGREVIEW:
-        return (
-          <Tag
-            className="border-success text-success"
-            variant="outline"
-            size={props.size}
-          >
-            {EventStatus.WAITINGREVIEW}
-          </Tag>
-        );
-      case EventStatus.COMPLETED:
-        return (
-          <Tag
-            className="border-success text-success"
-            variant="outline"
-            size={props.size}
-          >
-            {EventStatus.COMPLETED}
-          </Tag>
-        );
-      default:
-        return (
-          <Tag variant="outline" size={props.size}>
-            {props.status}
-          </Tag>
-        );
-    }
-  };
+export function StatusTag(props: StatusProps) {
+  switch (props.status) {
+    case EventStatus.WAITINGREVIEW:
+      return (
+        <Tag
+          className="border-success text-success"
+          variant="outline"
+          size={props.size}
+        >
+          {EventStatus.WAITINGREVIEW}
+        </Tag>
+      );
+    case EventStatus.COMPLETED:
+      return (
+        <Tag
+          className="border-success text-success"
+          variant="outline"
+          size={props.size}
+        >
+          {EventStatus.COMPLETED}
+        </Tag>
+      );
+    default:
+      return (
+        <Tag variant="outline" size={props.size}>
+          {props.status}
+        </Tag>
+      );
+  }
+}
