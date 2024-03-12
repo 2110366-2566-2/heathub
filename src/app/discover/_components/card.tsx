11bba0ffdfa34ad35226d100edfb04c47a00fd4a
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2024-03-12 23:28:43 +0700

    fix: discover design (finish)

--- src/app/discover/_components/card.tsx
+++ src/app/discover/_components/card.tsx
@@ -1,31 +1,79 @@
 "use client";
-import { tagIcon } from "@/utils/icon-mapping";
 import Image from "next/image";
 import { useMediaQuery } from "react-responsive";
-import { Tag } from "../../_components/tag";
-import Chat from "./chat";
 import { type ProfilePreviewProps } from "../types";
+import { tagStyle } from "@/utils/icon-mapping";
+import { Tag } from "@/app/_components/tag";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { RatingIcon } from "./rating-icon";
 
 export default function Card(props: ProfilePreviewProps) {
-  const { aka, age, image, interests } = props;
+  const { aka, age, image, interests, rating } = props;
 
   const isMobile = useMediaQuery({ maxWidth: 1023 });
-  const isLaptop = useMediaQuery({ maxWidth: 1279 });
-  const maxInterestsToDisplay = isMobile ? 3 : isLaptop ? 4 : 5;
+  const maxInterestsToDisplay = isMobile ? 3 : 4;
 
   const visibleInterests = interests.slice(0, maxInterestsToDisplay);
   const hiddenInterestsCount = interests.length - maxInterestsToDisplay;
 
   return (
-    <div className="relative flex aspect-[0.76] min-h-[424px] max-w-[323px] flex-col gap-0 rounded-3xl bg-white p-4 shadow-md">
+    <div className="relative flex aspect-[0.76] min-h-[424px] max-w-[323px] flex-col gap-3 rounded-3xl border-2 border-solid border-neutral-300 bg-white p-4">
+      {/* <FontAwesomeIcon
+        icon={faCertificate}
+        className="absolute right-[-28px] top-[-24px] z-40 h-16 w-24 text-[#FFC661]"
+      />
+      <div className="h3 absolute right-0 top-[-12px] z-50 rotate-12 flex-col font-extrabold text-white">
+        {rating.toFixed(1)}
+      </div>
+      <div className="absolute right-[11px] top-[14px] z-50 rotate-12 flex-col text-[8px] font-extrabold text-white">
+        rating
+      </div> */}
+      <div className="absolute right-[-28px] top-[-24px] z-40 rotate-12">
+        <RatingIcon rating={rating} />
+      </div>
+
       <div className="aspect-9/10 relative h-full max-h-[264px]">
         <Image
           src={image}
           alt="card"
           fill
           className="rounded-lg object-cover object-top"
+          draggable="false"
         />
       </div>
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-row gap-2">
+          <div className="h2 self-start font-bold text-high">
+            {aka}, {age}
+          </div>
+          <FontAwesomeIcon
+            icon={faCheckCircle}
+            className="h-6 w-6 self-center text-secondary-500"
+          />
+        </div>
+        <div className="flex flex-row flex-wrap items-center justify-start gap-2 self-stretch">
+          {visibleInterests.sort().map((tag, index) => {
+            return (
+              <Tag
+                key={index}
+                variant="solid"
+                icon={tagStyle[tag].icon}
+                size="md"
+                color={tagStyle[tag].color}
+              >
+                {tag}
+              </Tag>
+            );
+          })}
+          {hiddenInterestsCount > 0 && (
+            <Tag key="hidden-interests" variant="solid" size="md">
+              +{hiddenInterestsCount} more
+            </Tag>
+          )}
+        </div>
+      </div>
+
       {/* aspect-[0.76]<div className="relative h-full">
         <Image
           src={image}
--- src/utils/icon-mapping.ts
+++ src/utils/icon-mapping.ts
@@ -17,65 +17,65 @@ import {
 export const tagStyle = {
   Football: {
     icon: faFutbol,
-    color: "green"
+    color: "bg-tag-green",
   },
   Music: {
     icon: faMusic,
-    color: "red",
+    color: "bg-tag-red",
   },
   "K-pop": {
     icon: faMusic,
-    color: "red",
+    color: "bg-tag-red",
   },
   Movie: {
     icon: faFilm,
-    color: "pink",
+    color: "bg-tag-pink",
   },
   Anime: {
     icon: faFilm,
-    color: "pink",
+    color: "bg-tag-pink",
   },
   Book: {
     icon: faBook,
-    color: "blue",
+    color: "bg-tag-blue",
   },
   Fashion: {
     icon: faShirt,
-    color: "pink"
+    color: "bg-tag-pink",
   },
   Animal: {
     icon: faPaw,
-    color: "yellow",
+    color: "bg-tag-yellow",
   },
   Gym: {
     icon: faDumbbell,
-    color: "green",
+    color: "bg-tag-green",
   },
   Photography: {
     icon: faCamera,
-    color: "blue",
+    color: "bg-tag-blue",
   },
   Gaming: {
     icon: faGamepad,
-    color: "blue",
+    color: "bg-tag-blue",
   },
   Coffee: {
     icon: faMugHot,
-    color: "yellow",
+    color: "bg-tag-yellow",
   },
   Arts: {
     icon: faPalette,
-    color: "blue"
+    color: "bg-tag-blue",
   },
   Camping: {
     icon: faTents,
-    color: "yellow",
+    color: "bg-tag-yellow",
   },
   Basketball: {
     icon: faBasketball,
-    color: "green",
+    color: "bg-tag-green",
   },
-  }as const;
+} as const;
 
 export const tagList = Object.keys(tagStyle) as TagList;
 
