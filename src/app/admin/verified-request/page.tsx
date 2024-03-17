97d596c6851ccbafbfe5735cb35d7ff6c79ed577
bruce <bruceoio@hotmail.com>
2024-03-17 19:03:11 +0700

    feat: finish

---
+++ src/app/admin/verified-request/page.tsx
@@ -0,0 +1,140 @@
+"use client";
+import {
+  type VerifiedRequest,
+  requestVerifiedDataTableColumns,
+} from "../_components/RequestVerifiedDataTableColumns";
+import { RequestVerifiedDataTable } from "../_components/RequestVerifiedDataTable";
+import { faUserCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { api } from "@/trpc/react";
+import { useState } from "react";
+
+// const mockData: VerifiedRequest[] = [
+//   {
+//     requestDate: new Date(),
+//     requestId: 1,
+//     firstName: "Bruce",
+//     lastName: "nun",
+//     hostId: "123",
+//     userName: "Bruce",
+//     profileImageURL: "https://google.com/",
+//     nationalIdCardImageURL: "http://facebook.com/",
+//   },
+//   {
+//     requestDate: new Date(),
+//     requestId: 2,
+//     firstName: "Bruce",
+//     hostId: "123",
+//     lastName: "nun",
+//     userName: "Bruce",
+//     profileImageURL: "https://google.com/",
+//     nationalIdCardImageURL: "http://facebook.com/",
+//   },
+//   {
+//     requestDate: new Date(),
+//     requestId: 3,
+//     firstName: "Bruce",
+//     lastName: "nun",
+//     hostId: "123",
+//     userName: "Bruce",
+//     profileImageURL: "https://google.com",
+//     nationalIdCardImageURL: "http://facebook.com",
+//   },
+//
+//   {
+//     requestDate: new Date(),
+//     requestId: 1,
+//     hostId: "123",
+//     firstName: "Bruce",
+//     lastName: "nun",
+//     userName: "Bruce",
+//     profileImageURL: "https://google.com",
+//     nationalIdCardImageURL: "http://facebook.com",
+//   },
+//   {
+//     requestDate: new Date(),
+//     requestId: 2,
+//     firstName: "Bruce",
+//     lastName: "nun",
+//     hostId: "123",
+//     userName: "Bruce",
+//     profileImageURL: "https://google.com",
+//     nationalIdCardImageURL: "http://facebook.com",
+//   },
+//   {
+//     requestDate: new Date(),
+//     requestId: 3,
+//     firstName: "Bruce",
+//     lastName: "nun",
+//     userName: "Bruce",
+//     profileImageURL: "https://google.com",
+//     hostId: "123",
+//     nationalIdCardImageURL: "http://facebook.com",
+//   },
+// ];
+export default function Page() {
+  const [page, setPage] = useState<number>(0);
+  const [recentData, setRecentData] = useState<VerifiedRequest[]>([]);
+  const setPageByAction = (action: "next" | "previous") => {
+    if (action === "next") {
+      setPage((prev) => prev + 1);
+    } else if (page >= 1) {
+      setPage((prev) => prev - 1);
+    }
+  };
+
+  const { data } = api.admin.getVerifiedRequest.useQuery(
+    {
+      limit: 2,
+      page: page,
+    },
+    {
+      onSuccess: (data) => {
+        if (!data) return;
+        const formattedData = data.items.map((e) => {
+          const d: VerifiedRequest = {
+            requestDate: e.createdAt,
+            requestId: e.id,
+            hostId: e.hostID,
+            firstName: e.host.firstName,
+            lastName: e.host.lastName,
+            userName: e.host.aka,
+            profileImageURL: e.host.profileImageURL!,
+            nationalIdCardImageURL: e.nationalIDCardImageURL,
+          };
+          return d;
+        });
+        setRecentData(formattedData);
+      },
+    },
+  );
+  return (
+    <div className="flex flex-row">
+      <div className="h-screen w-[200px] min-w-[200px] bg-red-500">hello</div>
+      <div className=" flex w-full flex-col gap-4 p-9">
+        <div className="flex flex-col">
+          <div className="flex flex-row items-center space-x-2">
+            <FontAwesomeIcon
+              className="text-secondary-500"
+              icon={faUserCheck}
+              width={20}
+              height={20}
+            />
+            <span className="h4 font-bold">Verified Request</span>
+          </div>
+          <span className="text-sm text-medium">
+            A descriptive body text comes here
+          </span>
+        </div>
+
+        <RequestVerifiedDataTable
+          columns={requestVerifiedDataTableColumns}
+          data={recentData}
+          hasPrev={page !== 0}
+          hasNext={data?.hasNextPage ?? false}
+          setPage={setPageByAction}
+        />
+      </div>
+    </div>
+  );
+}
