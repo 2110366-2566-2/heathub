7cee06f74f831d188af3b1aa508144582f6f3443
Fa2ijai <112095289+Fa2ijai@users.noreply.github.com>
2024-03-19 14:20:28 +0700

    fix: fix toast position (#108)

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "pointer-events-none fixed bottom-0 right-0 z-[100]  flex flex max-h-screen w-full flex-row flex-col justify-center gap-4 p-4 transition-transform lg:justify-end",
+      "pointer-events-none fixed bottom-0 right-0 z-[100]  flex  max-h-screen w-full flex-row justify-center gap-4 p-4 transition-transform lg:justify-end",
       className,
     )}
     {...props}
