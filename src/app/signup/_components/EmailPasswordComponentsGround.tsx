97a2edfb848a7f6ef98a0587f1ecb9f9725acbc5
Punnawat Lohanuit <punnawat58000@gmail.com>
2024-03-20 11:37:06 +0700

    fix: EmailPassword design

--- src/app/signup/_components/EmailPasswordComponentsGround.tsx
+++ src/app/signup/_components/EmailPasswordComponentsGround.tsx
@@ -1,3 +1,119 @@
+// "use client";
+
+// import {
+//   isHost,
+//   type Host,
+//   type Participant,
+//   type User,
+// } from "@/app/signup/interfaces";
+// import { Button } from "@/components/ui/button";
+// import { useRef, useState, type Dispatch, type SetStateAction } from "react";
+// import EmailPasswordBox from "./EmailPasswordBox";
+// import { api } from "@/trpc/react";
+
+// interface ComponentGroundProps {
+//   setData: (data: User) => void;
+//   setPage: Dispatch<
+//     SetStateAction<
+//       | "ChooseRole"
+//       | "EmailPassword"
+//       | "HostDetails"
+//       | "ParticipantDetails"
+//       | "HostInterest"
+//     >
+//   >;
+//   data: User;
+// }
+
+// export default function ComponentsGround(props: ComponentGroundProps) {
+//   const { setData, setPage, data } = props;
+
+//   const formRef = useRef<HTMLFormElement>(null);
+
+//   const [isValid, setIsValid] = useState<boolean>(false);
+//   const [emailNotice, setEmailNotice] = useState<string>("");
+
+//   const testEmail = api.auth.isEmailAlreadyExist.useMutation();
+
+//   const isEmailDup = async (email: string) => {
+//     return await testEmail.mutateAsync({
+//       email: email,
+//     });
+//   };
+
+//   const handleButtonClick = async () => {
+//     if (!formRef.current || !isValid) {
+//       return;
+//     }
+
+//     const formData = new FormData(formRef.current);
+//     const password = formData.get("Password") as string | null;
+//     const email = formData.get("Email") as string | null;
+
+//     try {
+//       if (!(await isEmailDup(email ? email : ""))) {
+//         if (isHost(data)) {
+//           const host: Host = {
+//             Firstname: "",
+//             Lastname: "",
+//             Username: "",
+//             Bio: "",
+//             DOB: new Date(),
+//             Gender: "",
+//             Email: email ? email : "",
+//             Password: password ? password : "",
+//             Interest: [],
+//             Image: null,
+//           };
+//           setData(host);
+//           setPage("HostDetails");
+//         } else {
+//           const participant: Participant = {
+//             Firstname: "",
+//             Lastname: "",
+//             Username: "",
+//             DOB: new Date(),
+//             Gender: "",
+//             Email: email ? email : "",
+//             Password: password ? password : "",
+//             Image: null,
+//           };
+//           setData(participant);
+//           setPage("ParticipantDetails");
+//         }
+//       } else {
+//         setEmailNotice("This Email is already exits.");
+//       }
+//     } catch (error) {
+//       if (error instanceof Error) {
+//         setEmailNotice(error.message);
+//       } else {
+//         setEmailNotice("Something went wrong. Please try again.");
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center gap-y-6">
+//       <div className="h1 font-bold text-primary-900">Create your account</div>
+//       <EmailPasswordBox
+//         emailNotice={emailNotice}
+//         setEmailNotice={setEmailNotice}
+//         data={data}
+//         setValid={setIsValid}
+//         formRef={formRef}
+//       />
+//       <Button
+//         className="absolute bottom-6 h-12 w-[108px] bg-primary-500 text-white sm:static"
+//         variant="default"
+//         onClick={handleButtonClick}
+//       >
+//         Next
+//       </Button>
+//     </div>
+//   );
+// }
+
 "use client";
 
 import {
@@ -8,8 +124,13 @@ import {
 } from "@/app/signup/interfaces";
 import { Button } from "@/components/ui/button";
 import { useRef, useState, type Dispatch, type SetStateAction } from "react";
-import EmailPasswordBox from "./EmailPasswordBox";
 import { api } from "@/trpc/react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/utils/tailwind-merge";
+import * as React from "react";
+import { useEffect } from "react";
+import { z } from "zod";
 
 interface ComponentGroundProps {
   setData: (data: User) => void;
@@ -25,12 +146,14 @@ interface ComponentGroundProps {
   data: User;
 }
 
+const emailSchema = z.string().email();
+
 export default function ComponentsGround(props: ComponentGroundProps) {
   const { setData, setPage, data } = props;
 
   const formRef = useRef<HTMLFormElement>(null);
 
-  const [isValid, setIsValid] = useState<boolean>(false);
+  const [isValid, setValid] = useState<boolean>(false);
   const [emailNotice, setEmailNotice] = useState<string>("");
 
   const testEmail = api.auth.isEmailAlreadyExist.useMutation();
@@ -41,7 +164,68 @@ export default function ComponentsGround(props: ComponentGroundProps) {
     });
   };
 
+  const [emailText, setEmailText] = useState(data.Email ? data.Email : "");
+  const [passwordText, setPasswordText] = useState(
+    data.Password ? data.Password : "",
+  );
+  const [cfPasswordText, setCfPasswordText] = useState(
+    data.Password ? data.Password : "",
+  );
+
+  const [passwordNotice, setPasswordNotice] = useState<string>("");
+
+  const [confirmPasswordNotice, setConfirmPasswordNotice] =
+    useState<string>("");
+
+  const formCheck = () => {
+    console.log("formCheck");
+    if (emailNotice != "This Email is already exits.") {
+      setEmailNotice("");
+    }
+    setPasswordNotice("");
+    setConfirmPasswordNotice("");
+    if (!formRef.current) {
+      return;
+    }
+    const formData = new FormData(formRef.current);
+    const email = formData.get("Email") as string | null;
+    const password = formData.get("Password") as string | null;
+    const confirmPassword = formData.get("Confirm Password") as string | null;
+
+    let valid = false;
+
+    if (email && !emailSchema.safeParse(email).success) {
+      setValid(false);
+      setEmailNotice("Invalid email");
+      valid = false;
+    }
+
+    if (password && password.length < 8) {
+      setValid(false);
+      setPasswordNotice("Password must be at least 8 characters");
+      valid = false;
+    }
+
+    if (password !== confirmPassword) {
+      setValid(false);
+      setConfirmPasswordNotice("Passwords do not match");
+      valid = false;
+    }
+
+    if (!email || !password || !confirmPassword) {
+      console.log("Invalid");
+      setValid(false);
+    }
+
+    setValid(valid);
+  };
+
+  useEffect(() => {
+    formCheck();
+  });
+
   const handleButtonClick = async () => {
+    console.log(isValid);
     if (!formRef.current || !isValid) {
       return;
     }
@@ -94,22 +278,80 @@ export default function ComponentsGround(props: ComponentGroundProps) {
   };
 
   return (
-    <div className="flex flex-col items-center gap-y-6">
-      <div className="h1 font-bold text-primary-900">Create your account</div>
-      <EmailPasswordBox
-        emailNotice={emailNotice}
-        setEmailNotice={setEmailNotice}
-        data={data}
-        setValid={setIsValid}
-        formRef={formRef}
-      />
-      <Button
-        className="absolute bottom-6 h-12 w-[108px] bg-primary-500 text-white sm:static"
-        variant="default"
-        onClick={handleButtonClick}
+    <div className="flex w-full flex-col items-center gap-y-8 md:max-w-[506px]">
+      <div className="h1 text-center font-bold text-primary-900">
+        Create your account
+      </div>
+      <form
+        ref={formRef}
+        className="flex w-full flex-col items-center gap-y-10"
       >
-        Next
-      </Button>
+        <div className="flex w-full flex-col items-center gap-y-6">
+          <div className="flex w-full flex-col gap-y-1.5">
+            <Label htmlFor="Email" className="font-semibold">
+              Email
+            </Label>
+            <Input
+              value={emailText}
+              type="text"
+              name="Email"
+              placeholder="Enter your Email"
+              onKeyUp={formCheck}
+              onChange={(e) => {
+                setEmailText(e.currentTarget.value);
+              }}
+            />
+            <span className="text-sm text-red-500">{emailNotice}</span>
+          </div>
+          <div className="flex w-full flex-col gap-y-1.5">
+            <Label htmlFor="Password" className="font-semibold">
+              Password
+            </Label>
+            <Input
+              value={passwordText}
+              type="password"
+              name="Password"
+              placeholder="Enter your password"
+              onKeyUp={formCheck}
+              onChange={(e) => {
+                setPasswordText(e.currentTarget.value);
+              }}
+            />
+            <div
+              className={cn("text-sm text-placeholder ", {
+                "text-red-500": passwordNotice,
+              })}
+            >
+              The password must be at least 8 characters
+            </div>
+          </div>
+          <div className="flex w-full flex-col gap-y-1.5">
+            <Label htmlFor="Confirm Password" className="font-semibold">
+              Confirm Password
+            </Label>
+            <Input
+              value={cfPasswordText}
+              type="password"
+              name="Confirm Password"
+              placeholder="Enter your password"
+              onKeyUp={formCheck}
+              onChange={(e) => {
+                setCfPasswordText(e.currentTarget.value);
+              }}
+            />
+            <span className="text-sm text-red-500">
+              {confirmPasswordNotice}
+            </span>
+          </div>
+        </div>
+        <Button
+          className="h-12 w-full bg-primary-500 text-white sm:static"
+          variant="default"
+          onClick={handleButtonClick}
+        >
+          Next
+        </Button>
+      </form>
     </div>
   );
 }
--- src/app/signup/_components/_pages/EmailPassword.tsx
+++ src/app/signup/_components/_pages/EmailPassword.tsx
@@ -1,5 +1,6 @@
 import { type Dispatch, type SetStateAction } from "react";
 import { type User } from "../../interfaces";
+import Image from "next/image";
 import GoBackArrow from "../BackArrow";
 import ComponentsGround from "../EmailPasswordComponentsGround";
 
@@ -21,9 +22,43 @@ interface EmailPasswordProps {
 export default function EmailPassword(props: EmailPasswordProps) {
   const { setData, setPage, page, data } = props;
   return (
-    <div className="min-w-screen h-full min-h-screen w-full overflow-hidden bg-primary-50 p-6 md:p-9">
-      <GoBackArrow Page={page} setPage={setPage} />
-      <ComponentsGround data={data} setData={setData} setPage={setPage} />
+    <div className="flex min-h-screen">
+      <div className="hidden flex-[1_1_555px] bg-gradient-to-b from-primary-300 to-secondary-200 md:block">
+        <Quote />
+      </div>
+
+      <div className="w-full flex-[2_2_725px] bg-bgColor bg-none">
+        <main className="flex min-h-screen w-full flex-col">
+          <div className="flex w-full flex-col items-center gap-y-6 p-5 md:gap-y-[140px] md:p-6">
+            <div className="flex w-full flex-row">
+              <GoBackArrow Page={page} setPage={setPage} />
+            </div>
+            <ComponentsGround data={data} setData={setData} setPage={setPage} />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+function Quote() {
+  return (
+    <div className="mb-[-10px] flex min-h-0 flex-col items-center justify-center md:h-screen">
+      <Image
+        src="/images/signin/Group.png"
+        width={565}
+        height={338}
+        className="ml-[-50px]"
+        alt="Picture"
+      />
+      <div className="flex flex-col justify-center text-center">
+        <div className="md:h1 h2 font-bold text-primary-900 md:font-bold">
+          Find Your Perfect
+        </div>
+        <div className="md:h1 h2 font-bold text-primary-900 md:font-bold">
+          Companion Today!
+        </div>
+      </div>
     </div>
   );
 }
