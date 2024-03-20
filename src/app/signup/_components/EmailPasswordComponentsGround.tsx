5a7340df68a79613bba326769b5d977091fc8f2c
Punnawat Lohanuit <punnawat58000@gmail.com>
2024-03-20 11:37:06 +0700

    fix: EmailPassword design

--- src/app/signup/_components/EmailPasswordComponentsGround.tsx
+++ src/app/signup/_components/EmailPasswordComponentsGround.tsx
@@ -1,3 +1,119 @@
+// "use client";
+
+// import {
+//   isHost,
+//   type Host,
+//   type Participant,
+//   type User,
+// } from "@/app/signup/interfaces";
+// import { Button } from "@/components/ui/button";
+// import { useRef, useState, type Dispatch, type SetStateAction } from "react";
+// import EmailPasswordBox from "./EmailPasswordBox";
+// import { api } from "@/trpc/react";
+
+// interface ComponentGroundProps {
+//   setData: (data: User) => void;
+//   setPage: Dispatch<
+//     SetStateAction<
+//       | "ChooseRole"
+//       | "EmailPassword"
+//       | "HostDetails"
+//       | "ParticipantDetails"
+//       | "HostInterest"
+//     >
+//   >;
+//   data: User;
+// }
+
+// export default function ComponentsGround(props: ComponentGroundProps) {
+//   const { setData, setPage, data } = props;
+
+//   const formRef = useRef<HTMLFormElement>(null);
+
+//   const [isValid, setIsValid] = useState<boolean>(false);
+//   const [emailNotice, setEmailNotice] = useState<string>("");
+
+//   const testEmail = api.auth.isEmailAlreadyExist.useMutation();
+
+//   const isEmailDup = async (email: string) => {
+//     return await testEmail.mutateAsync({
+//       email: email,
+//     });
+//   };
+
+//   const handleButtonClick = async () => {
+//     if (!formRef.current || !isValid) {
+//       return;
+//     }
+
+//     const formData = new FormData(formRef.current);
+//     const password = formData.get("Password") as string | null;
+//     const email = formData.get("Email") as string | null;
+
+//     try {
+//       if (!(await isEmailDup(email ? email : ""))) {
+//         if (isHost(data)) {
+//           const host: Host = {
+//             Firstname: "",
+//             Lastname: "",
+//             Username: "",
+//             Bio: "",
+//             DOB: new Date(),
+//             Gender: "",
+//             Email: email ? email : "",
+//             Password: password ? password : "",
+//             Interest: [],
+//             Image: null,
+//           };
+//           setData(host);
+//           setPage("HostDetails");
+//         } else {
+//           const participant: Participant = {
+//             Firstname: "",
+//             Lastname: "",
+//             Username: "",
+//             DOB: new Date(),
+//             Gender: "",
+//             Email: email ? email : "",
+//             Password: password ? password : "",
+//             Image: null,
+//           };
+//           setData(participant);
+//           setPage("ParticipantDetails");
+//         }
+//       } else {
+//         setEmailNotice("This Email is already exits.");
+//       }
+//     } catch (error) {
+//       if (error instanceof Error) {
+//         setEmailNotice(error.message);
+//       } else {
+//         setEmailNotice("Something went wrong. Please try again.");
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center gap-y-6">
+//       <div className="h1 font-bold text-primary-900">Create your account</div>
+//       <EmailPasswordBox
+//         emailNotice={emailNotice}
+//         setEmailNotice={setEmailNotice}
+//         data={data}
+//         setValid={setIsValid}
+//         formRef={formRef}
+//       />
+//       <Button
+//         className="absolute bottom-6 h-12 w-[108px] bg-primary-500 text-white sm:static"
+//         variant="default"
+//         onClick={handleButtonClick}
+//       >
+//         Next
+//       </Button>
+//     </div>
+//   );
+// }
+
 "use client";
 
 import {
@@ -108,6 +224,7 @@ export default function ComponentsGround(props: ComponentGroundProps) {
   });
 
   const handleButtonClick = async () => {
+    console.log(isValid);
     if (!formRef.current || !isValid) {
       return;
     }
