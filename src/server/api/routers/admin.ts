8ae33b13466b2613df73f74b42f003421dabb19e
Brucennnnn <30439608+Brucennnnn@users.noreply.github.com>
2024-03-25 20:54:59 +0700

    feat: add delete nationalIdImage (#142)

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -1,4 +1,5 @@
 import { createTRPCRouter, adminProcedure } from "@/server/api/trpc";
+import { removeUploadthingURLPrefix, utapi } from "@/app/api/uploadthing/core";
 
 import {
   eventReport,
@@ -22,6 +23,10 @@ export const adminRouter = createTRPCRouter({
       }),
     )
     .mutation(async ({ ctx, input }) => {
+      let verifiedReq = await ctx.db.query.verifiedRequest.findFirst({
+        where: eq(verifiedRequest.id, input.requestID),
+        columns: { id: true, nationalIDCardImageURL: true },
+      });
       await ctx.db.transaction(async (tx) => {
         await tx
           .update(hostUser)
@@ -43,6 +48,14 @@ export const adminRouter = createTRPCRouter({
             ),
           );
       });
+      if (
+        verifiedReq?.nationalIDCardImageURL &&
+        verifiedReq.nationalIDCardImageURL !== ""
+      ) {
+        await utapi.deleteFiles(
+          removeUploadthingURLPrefix(verifiedReq.nationalIDCardImageURL),
+        );
+      }
     }),
 
   getVerifiedRequest: adminProcedure
