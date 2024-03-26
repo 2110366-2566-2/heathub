447512433f7051caf42a579ce56b874012f072dc
bpremika <premika.yu@gmail.com>
2024-03-26 23:22:12 +0700

    fix: wrong navbar

--- src/app/_components/navItem.tsx
+++ src/app/_components/navItem.tsx
@@ -29,3 +29,18 @@ export function NavItem(prop: NavItemProp) {
     </Link>
   );
 }
+
+
+export function  NavItemMobile(prop: NavItemProp) {
+  return (
+    <Link
+      href={prop.link}
+      className={cn(
+        "group relative flex w-12 justify-center self-center rounded-lg p-2 text-invert hover:cursor-pointer hover:bg-secondary-300",
+        prop.isSelected && "bg-secondary-500",
+      )}
+    >
+      <FontAwesomeIcon icon={prop.icon} size={"2x"} />
+    </Link>
+  );
+}
