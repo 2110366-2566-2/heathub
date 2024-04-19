e86dd3ca8009f8f550d63bc4438e36b956fa0770
Teerapat Chantaramanee <teerapatchantaramanee@gmail.com>
2024-04-20 00:33:57 +0700

    feat: test resources

---
+++ cypress/support/utils.ts
@@ -0,0 +1,4 @@
+export const path = "https://heathub.vercel.app";
+export const validImagePath = "./cypress/fixtures/host_img.jpg";
+export const exceedFileSizeImagePath = "cypress/fixtures/exceed_img.jpg";
+export const notImageFileImagePath = "cypress/fixtures/text.txt";
