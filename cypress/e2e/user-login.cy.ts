0b4ab44dadc238701f586109ff5b12da94ccc2e9
Fa2ijai <112095289+Fa2ijai@users.noreply.github.com>
2024-04-19 21:31:48 +0700

    feat:add cypress fixture (#192)
    * feat: add ignore setupTests.ts
    
    * feat: add fixture

--- cypress/e2e/user-login.cy.ts
+++ cypress/e2e/user-login.cy.ts
@@ -1,19 +1,23 @@
+import { user } from "@/server/db/schema";
+
 describe("Login Test", () => {
   it("TC2-1 Valid Email and Correct Password", () => {
     cy.visit("https://heathub.vercel.app/signin");
     cy.wait(1000);
-    //Change to happy@gmail.com
-    cy.get("input[type=email]").type("faijai@gmail.com");
-    //Chgange to test1234
-    cy.get("input[type=password]").type("fai1234567");
+    cy.fixture("loginData").then((user) => {
+      cy.get("input[type=email]").type(user.email);
+      cy.get("input[type=password]").type(user.password);
+    });
     cy.get("button[type=submit]").click();
-    cy.wait(1000);
+    cy.wait(3000);
     cy.contains("Welcome back").should("exist");
   });
   it("TC2-2 Email is empty", () => {
     cy.visit("https://heathub.vercel.app/signin");
     cy.wait(1000);
-    cy.get("input[type=password]").type("test1234");
+    cy.fixture("loginData").then((user) => {
+      cy.get("input[type=password]").type(user.password);
+    });
     cy.get("button[type=submit]").click();
     cy.wait(1000);
     cy.contains("Welcome back").should("not.exist");
@@ -21,7 +25,9 @@ describe("Login Test", () => {
   it("TC2-3 Password is empty", () => {
     cy.visit("https://heathub.vercel.app/signin");
     cy.wait(1000);
-    cy.get("input[type=email]").type("happy@gmail.com");
+    cy.fixture("loginData").then((user) => {
+      cy.get("input[type=email]").type(user.email);
+    });
     cy.get("button[type=submit]").click();
     cy.wait(1000);
     cy.contains("Welcome back").should("not.exist");
@@ -29,8 +35,10 @@ describe("Login Test", () => {
   it("TC2-4 Email is valid and does not exist in the system", () => {
     cy.visit("https://heathub.vercel.app/signin");
     cy.wait(1000);
-    cy.get("input[type=email]").type("nothappy@gmail.com");
-    cy.get("input[type=password]").type("test1234");
+    cy.fixture("loginData").then((user) => {
+      cy.get("input[type=email]").type(user.email2);
+      cy.get("input[type=password]").type(user.password);
+    });
     cy.get("button[type=submit]").click();
     cy.wait(1000);
     cy.contains("Welcome back").should("not.exist");
@@ -38,8 +46,10 @@ describe("Login Test", () => {
   it("TC2-5 Email is invalid", () => {
     cy.visit("https://heathub.vercel.app/signin");
     cy.wait(1000);
-    cy.get("input[type=email]").type("happygmail.com");
-    cy.get("input[type=password]").type("test1234");
+    cy.fixture("loginData").then((user) => {
+      cy.get("input[type=email]").type(user.invalid_email);
+      cy.get("input[type=password]").type(user.password);
+    });
     cy.get("button[type=submit]").click();
     cy.wait(1000);
     cy.contains("Welcome back").should("not.exist");
@@ -47,8 +57,10 @@ describe("Login Test", () => {
   it("TC2-6 Password is incorrect compare with the database system", () => {
     cy.visit("https://heathub.vercel.app/signin");
     cy.wait(1000);
-    cy.get("input[type=email]").type("happy@gmail.com");
-    cy.get("input[type=password]").type("abcd1234");
+    cy.fixture("loginData").then((user) => {
+      cy.get("input[type=email]").type(user.invalid_email);
+      cy.get("input[type=password]").type(user.password2);
+    });
     cy.get("button[type=submit]").click();
     cy.wait(1000);
     cy.contains("Welcome back").should("not.exist");
