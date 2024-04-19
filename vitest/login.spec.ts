79320690383146db8e285546548e91cf2c9ebd25
Fa2ijai <112095289+Fa2ijai@users.noreply.github.com>
2024-04-19 22:02:07 +0700

    fix: delete comment

--- vitest/login.spec.ts
+++ vitest/login.spec.ts
@@ -1,35 +1,3 @@
-// import { signIn } from "@/action/auth";
-// import { describe, test, expect, vi } from "vitest";
-
-// vi.mock("next/headers", () => ({
-//   cookies: vi.fn().mockReturnValue({ set: vi.fn() }), // Mock cookies function to return an set mock function object
-// }));
-
-// describe("signIn function", () => {
-//   // test("should return empty string when valid credentials are provided", async () => {
-//   //   const formData: FormData = new FormData();
-//   //   formData.append("email", "faija@gmail.com");
-//   //   formData.append("password", "fai1234567");
-//   //   const result = await signIn(formData);
-//   //   expect(result).toBe("");
-//   // });
-//   // test("email is empty", async () => {
-//   //   const formData: FormData = new FormData();
-//   //   formData.append("email", "");
-//   //   formData.append("password", "fai1234567");
-//   //   const result = await signIn(formData);
-//   //   expect(result).toBe("Invalid email or password");
-//   // });
-//   test("email is empty", async () => {
-//     const formData: FormData = new FormData();
-//     formData.append("email", null as any);
-//     formData.append("password", "fai1234567");
-//     const result = await signIn(formData);
-//     // expect(result).toThrow(Error("Missing email or password"));
-//     expect(result).toBe("Invalid email or password");
-//   });
-// });
-
 import { signIn } from "@/action/auth";
 import { describe, test, beforeEach, expect, vi } from "vitest";
 import { auth } from "@/server/api/auth";
