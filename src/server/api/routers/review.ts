b7eceb6efe65193d85c166bce162587852384158
Chawin Chitsangalert <111551141+oOWinOo@users.noreply.github.com>
2024-04-20 21:30:58 +0700

    fix(api) : change review counting method. (#198)

--- src/server/api/routers/review.ts
+++ src/server/api/routers/review.ts
@@ -75,17 +75,14 @@ export const reviewRouter = createTRPCRouter({
 
       const avgF = parseFloat(avgScore.avg);
       const roundedAvg = Math.round(avgF * 10) / 10;
-      const hostData = await ctx.db.query.hostUser.findFirst({
-        where: eq(hostUser.userID, input.hostID),
+      const reviews = await ctx.db.query.ratingAndReview.findMany({
+        where: eq(ratingAndReview.hostID, input.hostID),
       });
-      if (!hostData) {
-        return;
-      }
       await ctx.db
         .update(hostUser)
         .set({
           avgRating: roundedAvg,
-          reviewCount: (hostData?.reviewCount ?? 0) + 1,
+          reviewCount: reviews.length,
         })
         .where(eq(hostUser.userID, input.hostID));
       return;
