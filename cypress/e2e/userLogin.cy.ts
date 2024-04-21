4f3de89f8054e9baf53490dcb814b2987fa2b168
Fa2ijai <112095289+Fa2ijai@users.noreply.github.com>
2024-04-21 22:22:29 +0700

    refactor: hostRegister and userLogin

--- cypress/e2e/userLogin.cy.ts
+++ cypress/e2e/userLogin.cy.ts
@@ -2,73 +2,58 @@ import { type User } from "../support/types";
 import { path } from "../support/utils";
 
 describe("Login Test", () => {
-  it("TC2-1 Valid Email and Correct Password", () => {
+  beforeEach(() => {
     cy.visit(path + "/signin");
-    cy.wait(1000);
+  });
+  it("TC2-1 Valid Email and Correct Password", () => {
     cy.fixture("/login/valid").then((user: User) => {
-      cy.get("input[type=email]").type(user.email);
+      cy.get("input[type=email]", { timeout: 1000 }).type(user.email);
       cy.get("input[type=password]").type(user.password);
     });
     cy.get("button[type=submit]").click();
-    cy.wait(1000);
-    cy.contains("Welcome back").should("exist");
+    cy.contains("Welcome back").should("exist", { timeout: 1000 });
   });
 
   it("TC2-2 Email is empty", () => {
-    cy.visit(path + "/signin");
-    cy.wait(1000);
     cy.fixture("/login/valid").then((user: User) => {
       cy.get("input[type=password]").type(user.password);
     });
     cy.get("button[type=submit]").click();
-    cy.wait(1000);
-    cy.contains("Welcome back").should("not.exist");
+    cy.contains("Welcome back").should("not.exist", { timeout: 1000 });
   });
 
   it("TC2-3 Password is empty", () => {
-    cy.visit(path + "/signin");
-    cy.wait(1000);
     cy.fixture("/login/valid").then((user: User) => {
-      cy.get("input[type=email]").type(user.email);
+      cy.get("input[type=email]", { timeout: 1000 }).type(user.email);
     });
     cy.get("button[type=submit]").click();
-    cy.wait(1000);
-    cy.contains("Welcome back").should("not.exist");
+    cy.contains("Welcome back").should("not.exist", { timeout: 1000 });
   });
 
   it("TC2-4 Email is valid and does not exist in the system", () => {
-    cy.visit(path + "/signin");
-    cy.wait(1000);
     cy.fixture("/login/notExist").then((user: User) => {
-      cy.get("input[type=email]").type(user.email);
+      cy.get("input[type=email]", { timeout: 1000 }).type(user.email);
       cy.get("input[type=password]").type(user.password);
     });
     cy.get("button[type=submit]").click();
-    cy.wait(1000);
-    cy.contains("Welcome back").should("not.exist");
+    cy.contains("Welcome back").should("not.exist", { timeout: 1000 });
   });
 
   it("TC2-5 Email is invalid", () => {
-    cy.visit(path + "/signin");
-    cy.wait(1000);
     cy.fixture("/login/invalid").then((user: User) => {
-      cy.get("input[type=email]").type(user.email);
+      cy.get("input[type=email]", { timeout: 1000 }).type(user.email);
       cy.get("input[type=password]").type(user.password);
     });
     cy.get("button[type=submit]").click();
-    cy.wait(1000);
-    cy.contains("Welcome back").should("not.exist");
+    cy.contains("Welcome back").should("not.exist", { timeout: 1000 });
   });
 
   it("TC2-6 Password is incorrect compare with the database system", () => {
-    cy.visit(path + "/signin");
-    cy.wait(1000);
     cy.fixture("/login/passwordIncorrect").then((user: User) => {
-      cy.get("input[type=email]").type(user.email);
+      cy.get("input[type=email]", { timeout: 1000 }).type(user.email);
       cy.get("input[type=password]").type(user.password);
     });
     cy.get("button[type=submit]").click();
-    cy.wait(1000);
-    cy.contains("Welcome back").should("not.exist");
+    cy.contains("Welcome back").should("not.exist", { timeout: 1000 });
   });
 });
